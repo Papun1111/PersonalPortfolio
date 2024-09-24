93a496a2c9c645d385eb628180b7a075659392c6
Papun1111 <gohanmohapatra@gmail.com>
2024-09-24 21:26:23 +0530

    added projects

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,26 +1,40 @@
-import project1_img from '../assets/project1_img.png'
-import project2_img from '../assets/project2_img.png'
-import project3_img from '../assets/project3_img.png'
-
+import project1_img from "../assets/project1_img.png";
+import project2_img from "../assets/project2_img.png";
+import project3_img from "../assets/project3_img.png";
+import project4_img from "../assets/projectimg4.png";
+import project5_img from "../assets/projectimg4admin.png"
 const mywork_data = [
-    {
-        w_no:1,
-        w_name:"Video Calling App",
-        w_img:project1_img,
-        w_link:"https://gusiondaggercallsvideoapp.onrender.com/"
-    },
-    {
-        w_no:2,
-        w_name:"Zerodha Clone",
-        w_img:project2_img,
-        w_link:"https://zerodhariyalweb.onrender.com/"
-    },
-    {
-        w_no:3,
-        w_name:"WanderLust",
-        w_img:project3_img,
-        w_link:"https://wander-lust-skibidi.onrender.com/listings"
-    },
-]
- 
+  {
+    w_no: 1,
+    w_name: "Pamotra-Delivery",
+    w_img: project4_img,
+    w_link: "https://pamotradelivery.onrender.com/",
+  },
+  {
+    w_no: 2,
+    w_name: "Pamotra-Del-admin",
+    w_img: project5_img,
+    w_link: "https://fooddeliveryadminpage.onrender.com/add",
+  },
+  {
+    w_no: 3,
+    w_name: "Video Calling App",
+    w_img: project1_img,
+    w_link: "https://gusiondaggercallsvideoapp.onrender.com/",
+  },
+  {
+    w_no: 4,
+    w_name: "Zerodha Clone",
+    w_img: project2_img,
+    w_link: "https://zerodhariyalweb.onrender.com/",
+  },
+  {
+    w_no: 5,
+    w_name: "WanderLust",
+    w_img: project3_img,
+    w_link: "https://wander-lust-skibidi.onrender.com/listings",
+  },
+  ,
+];
+
 export default mywork_data;
